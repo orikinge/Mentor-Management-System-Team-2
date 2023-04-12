9f0d4a49041f5fe1d1b8a16fdc00933be60b4a17
Segun Akanbi <akanbisegun1@gmail.com>
2023-04-12 04:54:21 +0100

    update frontend setup (#64)
    * chore: setup reconfiguration
    
    * fix code quality
    
    * fix conflicts
    
    * fix: code quality
    
    * fix conflict
    
    ---------
    
    Co-authored-by: Peculiar C. Umeh <35475543+peculiaruc@users.noreply.github.com>
    Co-authored-by: Emmanuel Daniel <emmsdan.inc@gmail.com>

--- frontend/mms-Admin/pages/_app.js
+++ frontend/mms-Admin/pages/_app.js
@@ -6,8 +6,6 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-       
-
         <style>{`body { background-color: #f9f9f9b8 !important; }`}</style>
       </Head>
       <Component {...pageProps} />
