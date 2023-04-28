30bcd8a2a6e7f16a18a887261ace6ba787242423
Emmanuel Daniel <emmsdan.inc@gmail.com>
2023-04-28 13:47:32 +0100

    node package

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,7 @@
 cd ../
 
-./ngrok config add-authtoken 1PtDeQ85j9zN4zlTYP0LvAf39TV_6nQYvXrABzsuh52SZrpjY
+pm2 reload all
 
-./ngrok http 3333
+sudo ./ngrok config add-authtoken 1PtDeQ85j9zN4zlTYP0LvAf39TV_6nQYvXrABzsuh52SZrpjY
+
+sudo ./ngrok http 3333
